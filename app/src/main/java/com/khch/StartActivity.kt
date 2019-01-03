3a6febafc468b5519bc4f20f236ee285e76324fb
kuanghaochuan <609779612@qq.com>
2019-01-03 20:22:49 +0800

    add start activity

--- app/src/main/java/com/khch/StartActivity.kt
+++ app/src/main/java/com/khch/StartActivity.kt
@@ -3,8 +3,10 @@ package com.khch
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.khch.androidshapes.MainActivity
 import com.khch.androidshapes.R
 import com.khch.animator.AnimatorActivity
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
@@ -12,7 +14,14 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        val intent = Intent(this, AnimatorActivity::class.java)
-        startActivity(intent)
+        activity_start_shape_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        activity_start_animator_btn.setOnClickListener {
+            val intent = Intent(this, AnimatorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
